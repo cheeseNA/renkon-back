0033a6866c09396dbffe738a94d871a013dc7197
cheeseNA <kurocat2000@gmail.com>
2023-09-19 22:11:49 +0200

    Add database.py and models.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
 from app.routers import room
 
+from sqlalchemy.orm import Session
+
+from .database import models
+from .database.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 app = FastAPI()
 
