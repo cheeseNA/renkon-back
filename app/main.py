7f9bd680ae8124fd95be3dfc25d343477c9f2324
cheeseNA <kurocat2000@gmail.com>
2023-09-21 10:56:13 +0200

    Add contact and person endpoints

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from app.routers import room
 
 from app.database import models
 from app.database.database import engine
@@ -11,6 +10,8 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(room.router, prefix="/room", tags=["room"])
+app.include_router(person.router, prefix="/person", tags=["person"])
+app.include_router(contact.router, prefix="/contact", tags=["contact"])
 
 
 @app.get("/")
