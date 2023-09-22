2e95677ae365f39458a3894459651ceb3d5dd31d
cheeseNA <kurocat2000@gmail.com>
2023-09-22 15:30:56 +0200

    Add validation for duration of RoomCreate

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,12 +1,11 @@
 import datetime
 import uuid
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 from app.database.models import ContentType
 
 
-# TODO: validation, default values
 class Contact(BaseModel):
     id: int
     person_id: int
@@ -51,3 +50,15 @@ class Room(BaseModel):
 
 class RoomCreate(BaseModel):
     duration: datetime.timedelta
+
+    @field_validator("duration")
+    def duration_must_be_positive(cls, v):
+        if v <= datetime.timedelta():
+            raise ValueError("duration must be positive")
+        return v
+
+    @field_validator("duration")
+    def duration_must_be_less_than_3_days(cls, v):
+        if v > datetime.timedelta(days=3):
+            raise ValueError("duration must be less than 3 days")
+        return v
